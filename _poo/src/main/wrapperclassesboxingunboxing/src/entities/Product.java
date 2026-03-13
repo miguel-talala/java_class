ae1800a42ba7061d2a3d8b124603c3aae4ae17dc
Miguel Talala <migueltalalal09@gmail.com>
2026-03-12 22:52:38 -0300

    Enum

--- _poo/src/main/wrapperclassesboxingunboxing/src/entities/Product.java
+++
@@ -1,9 +0,0 @@
-package entities;
-// A good use of wrapper classes is allowing class attributes to be null,
-// giving flexibility when initializing objects
-public class Product {
-    public String name;
-    public Double price;
-    public Integer quantity;
-
-}
